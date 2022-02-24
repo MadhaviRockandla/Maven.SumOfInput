78462fc1ae9518d35053d71280ba7fb2265ce63e
Madhavi <madhavi@zipcoders-mbp.mynetworksettings.com>
2022-02-24 17:36:55 -0500

    Complted

---
+++ src/main/java/SumOfNumber.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * Created by iyasuwatts on 10/17/17.
+ */
+
+public class SumOfNumber {
+
+    public static void main(String[] args){
+
+        SumOfNumber main_object = new SumOfNumber();
+
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int newN = scanner.nextInt();
+
+        int result = main_object.sumOfNumbers(newN);
+        System.out.println(result);
+        scanner.close();
+
+    }
+
+    int sumOfNumbers(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    }
